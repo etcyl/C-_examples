3fbf07d2ea01662015d8e78b734539b3dbb65f66
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2018-07-16 18:26:47 -0700

    Update cs162_prog4main.cpp

--- cpp_examples/cs162_prog4main.cpp
+++ cpp_examples/cs162_prog4main.cpp
@@ -1,16 +1,17 @@
 //Preprocessor directives:
 #include "cs162_prog4.h"
 
-//Matt Fleetwood, CS 162, Program 4
-//This program uses a base struct to contain character arrays for information about a dog park (e.g. one array is for a park name,
-//another is for the park's location, another is for the park's description (is it muddy, hilly, are there benches), another for
-//whether the park is fenced (partially, completely, not fenced), and the last array is for the park size (small, medium, large).
-//This base struct's arrays are sized just right by dynamically allocating the arrays and getting the right size from the user's
-//entries (i.e. the user enters a park name "Park Name", and the length of that entry is used as the number for the struct array,
-//park name. Similarly, a class stores a dynamically allocated array of these structs, more of which can be added to this list
-//by using the class function addPark(). The class also has a constructor and destructor function, which initialize and delete the
-//class data members. The last two class functions are display functions, one of which displays a park's info by getting the user
-//to enter the park name and the other displays all of the parks and their info.
+/*Matt Fleetwood, CS 162, Program 4
+  This program uses a base struct to contain character arrays for information about a dog park (e.g. one array is for a park name,
+  another is for the park's location, another is for the park's description (is it muddy, hilly, are there benches), another for
+  whether the park is fenced (partially, completely, not fenced), and the last array is for the park size (small, medium, large).
+  This base struct's arrays are sized just right by dynamically allocating the arrays and getting the right size from the user's
+  entries (i.e. the user enters a park name "Park Name", and the length of that entry is used as the number for the struct array,
+  park name. Similarly, a class stores a dynamically allocated array of these structs, more of which can be added to this list
+  by using the class function addPark(). The class also has a constructor and destructor function, which initialize and delete the
+  class data members. The last two class functions are display functions, one of which displays a park's info by getting the user
+  to enter the park name and the other displays all of the parks and their info.
+*/
 
 int const TEXT = 31;
 
